885883cecb0f0dbb0d1d79f77b47043041f8a0fa
Kim-Bim-123 <113792671+Kim-Bim-123@users.noreply.github.com>
2023-01-08 19:40:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,39 @@ const express = require("express");
 const app = express();
 const postBank= require("./postBank");
 const morgan = require ("morgan");
-const path = require('path')
-postBank.find()
+const path = require('path');
+postBank.find();
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
+
+
+
+
+
+app.get('/posts/:id', (req, res, next) => {
+  const id = req.params.id
+  const post = postBank.find(id)
+  if (!post.id) {
+    // If the post wasn't found, set the HTTP status to 404 and send Not Found HTML
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
+        <img src="/dumbledore-404.gif" />
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+  } else{ next()}})
 
 app.get('/posts/:id', (req, res) => {
   const id = req.params.id;
